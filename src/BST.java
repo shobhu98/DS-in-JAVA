853af2852dff653d02d7100b1a903b5d2145fa83
shobhu98 <46121289+shobhu98@users.noreply.github.com>
2019-05-20 16:58:24 +0530

    h

--- src/BST.java
+++ src/BST.java
@@ -1,7 +1,33 @@
 public class BST {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("higi");
+    private Node root;
+
+   public BST(int[] arr){
+       root=constructor(arr,0,arr.length);
+
+   }
+   private Node constructor(int[] arr,int low,int high){
+       if(low>high){
+           return null ;
+       }
+       int mid=(low+high)/2;
+       Node node=new Node(arr[mid]);
+       node.left=constructor(arr,low,mid-1);
+       node.right=constructor(arr,mid+1,high);
+
+       return node;
+
+   }
+
+
+
+    private class Node{
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value){
+            this.value=value;
+        }
     }
 }
 

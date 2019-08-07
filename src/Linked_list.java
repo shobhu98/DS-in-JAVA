feb7e59c787a95f883b3e75c7725d2d71473ce8a
shobhu98 <46121289+shobhu98@users.noreply.github.com>
2019-08-08 00:25:18 +0530

    a

--- src/Linked_list.java
+++ src/Linked_list.java
@@ -3,68 +3,210 @@ public class Linked_list {
     private Node tail;
     private int size;
 
-    public void insertFirst(int value){
-        Node node=new Node(value);
-        node.next=head;
-        head=node;
-        if(size==0){
-            tail=head;
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (size == 0) {
+            tail = head;
         }
         size++;
 
     }
-    public void inserLast(int value){
-        if(size==0){
-            insertFirst(value);
+
+    public void insertLast(int val) {
+        if (size == 0) {
+            insertFirst(val);
             return;
         }
-        Node node=new Node(value);
-        tail.next=node;
-        tail=node;
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
         size++;
 
-    }
-    public void deleteFirst(){
-        Integer value=head.value;
-        head=head.next;
 
-        if(head==null){
-            tail=head;
+    }
+
+    public void deleteFirst() {
+        int val = head.value;
+        System.out.println(val);
+        head = head.next;
+        if (head == null) {
+            tail = head;
         }
+        size--;
+    }
+
+    public void deleteLast() {
+        Node node = head;
+        while (node.next.next != null) {
+            node = node.next;
+        }
+        node.next = null;
+        tail = node;
         size--;
 
     }
 
-    public void deleteLast(){
-        if(size<2){
-            deleteFirst();
-            return;
-        }
-        Integer value=tail.value;
-        Node node=head;
-        while ()
-    }
-    public void display(){
-        Node node=head;
-        while (node!=null){
-            System.out.print(node.value+",");
-            node=node.next;
+    public void display() {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.value + ",");
+            node = node.next;
 
         }
         System.out.print("\b end ");
     }
 
 
-
-
     private class Node {
-        private Integer value;
-        private  Node next;
+        private int value;
+        private Node next;
 
-        private Node(int value){
-            this.value=value;
+        private Node(int value) {
+            this.value = value;
         }
-
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class Linked_list {
+//    private Node head;
+//    private Node tail;
+//    private int size;
+//
+//    public void insertFirst(int value){
+//        Node node=new Node(value);
+//        node.next=head;
+//        head=node;
+//        if(size==0){
+//            tail=head;
+//        }
+//        size++;
+//
+//    }
+//    public void insertLast(int value){
+//        if(size==0){
+//            insertFirst(value);
+//            return;
+//        }
+//        Node node=new Node(value);
+//        tail.next=node;
+//        tail=node;
+//        size++;
+//
+//    }
+//    public void deleteFirst(){
+//        Integer value=head.value;
+//        System.out.println(value+"deleted");
+//        head=head.next;
+//
+//        if(head==null){
+//            tail=head;
+//        }
+//        size--;
+//
+//    }
+//
+//    public void deleteLast(){
+//        if(size<2){
+//            deleteFirst();
+//            return;
+//        }
+//        Integer value=tail.value;
+//        System.out.println(value+"deleted");
+//        Node node=head;
+//        while (node.next!=null){
+//            node=node.next;
+//
+//        }
+//
+//        node.next=null;
+//        tail=node;
+//        size--;
+//    }
+//    public void display(){
+//        Node node=head;
+//        while (node!=null){
+//            System.out.print(node.value+",");
+//            node=node.next;
+//
+//        }
+//        System.out.print("\b end ");
+//    }
+//
+//
+//
+//
+//    private class Node {
+//        private Integer value;
+//        private  Node next;
+//
+//        private Node(int value){
+//            this.value=value;
+//        }
+//
+//
+//    }
+//}
--- src/linked_client.java
+++ src/linked_client.java
@@ -1,9 +1,11 @@
 public class linked_client {
     public static void main(String[] args) {
         Linked_list list=new Linked_list();
-        list.inserLast(1);
-        list.inserLast(2);
-        list.inserLast(3);
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+//        list.deleteFirst();
+        list.deleteLast();
         list.display();
     }
 }
